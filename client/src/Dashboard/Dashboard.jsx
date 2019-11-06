272471431f34b29ac756b71524f4b1424bc7eded
Minh Lu <Minh.Lu45@T-mobile.com>
2019-11-06 12:32:54 -0800

    Encapsulate other functionality into methods, like checking for max users exceeded

--- client/src/Dashboard/Dashboard.jsx
+++ client/src/Dashboard/Dashboard.jsx
@@ -23,17 +23,9 @@ class Dashboard extends React.Component {
         });
     }
 
-    // So, every now and then, 
-
-    // May want API to return isFull...and max number of users allowed, and cost
-    // This is a bandaid for now
-    hasReachedUserLimit() {
-        return this.state.numberOfUsers == 100;
+    renderUserLimitExceededMessage() {
+        return this.state.numberOfUsers == this.state.maximumNumberOfUsersAllowed;
     }
-
-    // Reached maximum...That will be a component lifecycle check to constantly compare isFull = true?
-    // Once upgraded calling API upgrade, we turn currentPlan to two, in which case upgrade text is invoked
-    // Show upgrade to enterprise if startupplan = 1
 
     renderUpgradeButton() {
         if (this.state.currentPlan === 1) {
@@ -56,8 +48,13 @@ class Dashboard extends React.Component {
         }
     }
 
-    //<div className="alert is-error" >You have exceeded the maximum number of users for your account, please upgrade your plan to increaese the limit.</div>
-    //<div className="alert is-success">Your account has been upgraded successfully!</div>
+    renderUpgradeSuccessMessage() {
+        if (this.state.currentPlan === 2) {
+            return (
+                <div className="alert is-success">Your account has been upgraded successfully!</div>
+            );
+        }
+    }
     
     render() {
         return (
@@ -70,17 +67,18 @@ class Dashboard extends React.Component {
                 <button className="button is-border">Logout</button>
                 </header>
                 
-                
+                {this.renderUserLimitExceededMessage()}
+                {this.renderUpgradeSuccessMessage()}
 
                 <div className="plan">
                     {this.renderHeader()}
 
                     <div className="plan-content">
                         <div className="progress-bar">
-                        <div style={{width: 35 + '%'}} className="progress-bar-usage"></div>
+                        <div style={{width: (this.state.numberOfUsers/this.state.maximumNumberOfUsersAllowed) + '%'}} className="progress-bar-usage"></div>
                         </div>
 
-                        <h3>Users: {this.state.numberOfUsers}/100</h3>
+                        <h3>Users: {this.state.numberOfUsers}/{this.state.maximumNumberOfUsersAllowed}</h3>
                     </div>
 
                     <footer>
